9226a8cb2bb469fc2848cff0510b65bc1afe911e
Dave Ceddia <dceddia@gmail.com>
2017-01-08 01:38:51 -0500

    Improve 'shorten' algorithm + tests

--- src/components/Issue.js
+++ src/components/Issue.js
@@ -3,16 +3,30 @@ import UserWithAvatar from './UserWithAvatar';
 import IssueLabels from './IssueLabels';
 import './Issue.css';
 
-function shorten(text = "", length = 140) {
+export function shorten(text = "", maxLength = 140) {
   // Normalize newlines
   let cleanText = text.replace(/\\r\\n/g, "\n");
 
   // Return if short enough already
-  if(cleanText.length <= length) {
+  if(cleanText.length <= maxLength) {
     return cleanText;
   }
 
-  return cleanText.substr(0, 140);
+  // Return the 140 chars as-is if they end in a non-word char
+  const oneTooLarge = cleanText.substr(0, 141);
+  if(/\W$/.test(oneTooLarge)) {
+    return oneTooLarge.substr(0, 140) + "...";
+  }
+
+  // Walk backwards to the nearest non-word character
+  let i = oneTooLarge.length;
+  while(--i) {
+    if(/\W/.test(oneTooLarge[i])) {
+      return oneTooLarge.substr(0, i) + "...";
+    }
+  }
+
+  return oneTooLarge.substr(0, 140) + "...";
 }
 
 export default function Issue({ number, title, labels, user, summary }) {
--- src/components/Issue.test.js
+++ src/components/Issue.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { checkSnapshot } from '../testUtils';
-import Issue from './Issue';
+import Issue, { shorten } from './Issue';
 
 it('renders', () => {
   const tree = shallow(
@@ -20,4 +20,43 @@ it('renders', () => {
   );
 
   checkSnapshot(tree);
+});
+
+describe('shorten', () => {
+  it('leaves text alone if short enough', () => {
+    const justFine = 'a'.repeat(139);
+    expect(shorten(justFine)).toEqual(justFine);
+  });
+
+  it('shortens when there are no breaks', () => {
+    const tooLong = 'a'.repeat(141);
+    const expected = 'a'.repeat(140) + "...";
+    expect(shorten(tooLong)).toEqual(expected);
+  });
+
+  it('shortens to 140 when 141 is a non-word', () => {
+    const tooLong = 'a'.repeat(140) + " ";
+    const expected = 'a'.repeat(140) + "...";
+    expect(shorten(tooLong)).toEqual(expected);
+  });
+
+  describe('shortening more than necessary', () => {
+    const testWithChar = (char) => {
+      const tooLong = 'a'.repeat(135) + char + 'b'.repeat(50);
+      const expected = 'a'.repeat(135 )+ "...";
+      expect(shorten(tooLong)).toEqual(expected);
+    };
+
+    it('shortens to the nearest whitespace', () => {
+      testWithChar(' ');
+      testWithChar('\n');
+      testWithChar('\t');
+    });
+
+    it('shortens to the nearest punctuation', () => {
+      testWithChar('.');
+      testWithChar(';');
+      testWithChar('!');
+    });
+  })
 });
